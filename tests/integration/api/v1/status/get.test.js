722851ad17442d05c2e842c7dd384ce1dbc20536
João Fernandes <110104126+ja1rocambole@users.noreply.github.com>
2026-01-23 20:11:02 +0000

    finaliza o enpoint `api/v1/status`

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,13 @@
 test("GET to api/v1/status retorna status 200", async () => {
   const res = await fetch("http://localhost:3000/api/v1/status");
   expect(res.status).toBe(200);
+
+  const resBody = await res.json();
+
+  const parsedUpdatedAt = new Date(resBody.updated_at).toISOString();
+  expect(resBody.updated_at).toEqual(parsedUpdatedAt);
+
+  expect(resBody.dependencies.database.version).toEqual("16.0");
+  expect(resBody.dependencies.database.max_connections).toEqual(100);
+  expect(resBody.dependencies.database.opened_connections).toEqual(1);
 });
