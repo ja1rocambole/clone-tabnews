d35f5345a17e19db8e3b11cde256bb625d5b2d30
João Fernandes <110104126+ja1rocambole@users.noreply.github.com>
2026-06-05 19:51:38 +0000

    test: rename all descriptions to use new pattern

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -6,24 +6,31 @@ beforeAll(async () => {
   await database.query("DROP SCHEMA PUBLIC CASCADE; CREATE SCHEMA PUBLIC;");
 });
 
-test("POST to api/v1/migrations retorna status 200", async () => {
-  const res1 = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "POST",
+describe("POST /api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    describe("Running pending migrations", () => {
+      test("For first time", async () => {
+        const res1 = await fetch("http://localhost:3000/api/v1/migrations", {
+          method: "POST",
+        });
+        expect(res1.status).toBe(201);
+
+        const res1Body = await res1.json();
+
+        expect(Array.isArray(res1Body)).toBe(true);
+        expect(res1Body.length).toBeGreaterThan(0);
+      });
+      test("For second time", async () => {
+        const res2 = await fetch("http://localhost:3000/api/v1/migrations", {
+          method: "POST",
+        });
+        expect(res2.status).toBe(200);
+
+        const res2Body = await res2.json();
+
+        expect(Array.isArray(res2Body)).toBe(true);
+        expect(res2Body.length).toBe(0);
+      });
+    });
   });
-  expect(res1.status).toBe(201);
-
-  const res1Body = await res1.json();
-
-  expect(Array.isArray(res1Body)).toBe(true);
-  expect(res1Body.length).toBeGreaterThan(0);
-
-  const res2 = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "POST",
-  });
-  expect(res2.status).toBe(200);
-
-  const res2Body = await res2.json();
-
-  expect(Array.isArray(res2Body)).toBe(true);
-  expect(res2Body.length).toBe(0);
 });
