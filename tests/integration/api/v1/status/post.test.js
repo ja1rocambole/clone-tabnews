5f89b920fd063f5fe0c5c7cd7e9f7bd9b59a0a49
João Fernandes <110104126+ja1rocambole@users.noreply.github.com>
2026-06-19 21:02:59 +0000

    refactor: use `next-connect` in `/migrations` and `/status`

---
+++ tests/integration/api/v1/status/post.test.js
@@ -0,0 +1,27 @@
+import orchestrator from "tests/orchestrator.js";
+
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
+});
+
+describe("POST /api/v1/status", () => {
+  describe("Anonymous user", () => {
+    test("Retrieving current system status", async () => {
+      const res = await fetch("http://localhost:3000/api/v1/status", {
+        method: "POST",
+      });
+
+      expect(res.status).toBe(405);
+
+      const resBody = await res.json();
+
+      expect(resBody).toEqual({
+        name: "MethodNotAllowedError",
+        message: "Método não permitido para este endpoint.",
+        action:
+          "Verifique se o método HTTP enviado é válido para este endpoint.",
+        status_code: 405,
+      });
+    });
+  });
+});
