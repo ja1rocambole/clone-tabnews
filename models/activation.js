7b8e4f212075eb2cbd404d552653f515a0cce8b8
João Fernandes <110104126+ja1rocambole@users.noreply.github.com>
2026-08-24 23:43:03 +0000

    feat: require `read:activation_token` to acess `/api/v1/activations/[token_id]`

--- models/activation.js
+++ models/activation.js
@@ -2,7 +2,8 @@ import database from "infra/database.js";
 import webserver from "infra/webserver.js";
 import email from "infra/email.js";
 import user from "models/user.js";
-import { NotFoundError } from "infra/errors";
+import authorization from "models/authorization.js";
+import { ForbiddenError, NotFoundError } from "infra/errors";
 
 const EXPIRATION_IN_MILLISECONDS = 60 * 15 * 1000; // 15 minutes
 
@@ -100,6 +101,15 @@ async function markTokenAsUsed(activationTokenId) {
 }
 
 async function activateUserByUserId(userId) {
+  const userToActivate = await user.findOneById(userId);
+
+  if (!authorization.can(userToActivate, "read:activation_token")) {
+    throw new ForbiddenError({
+      message: "Você não pode mais utilizar tokens de ativação.",
+      action: "Entre em contato com o suporte.",
+    });
+  }
+
   const activatedUser = await user.setFeatures(userId, [
     "create:session",
     "read:session",
@@ -108,6 +118,7 @@ async function activateUserByUserId(userId) {
 }
 
 const activation = {
+  EXPIRATION_IN_MILLISECONDS,
   create,
   findOneValidById,
   sendEmailToUser,
